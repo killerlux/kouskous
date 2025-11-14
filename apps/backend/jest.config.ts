750a87b951d4cc359eba3117f97f8d5bbf34a705
Aymen Kouskoussi <aymen.kouskoussi@gmail.com>
2025-11-14 16:33:55 +0100

    test: ensure backend uses ts-jest config

---
+++ apps/backend/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.(t|j)s',
+    '!src/main.ts',
+  ],
+  coverageDirectory: '../coverage',
+};
+
+export default config;
+
